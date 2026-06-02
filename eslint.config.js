a702d86ae35a7de95b515a79ddda216622b8e602
Samuel <me@sammtts.com>
2026-06-01 19:28:43 -0500

    refactor(eslint.config.js): remove Prettier recommended config from ESLint setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
 import js from '@eslint/js';
-import prettierRecommended from 'eslint-plugin-prettier/recommended';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
 
 export default [
   js.configs.recommended,
-  prettierRecommended,
 
   {
     languageOptions: {
